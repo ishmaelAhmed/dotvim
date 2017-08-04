224520c7d5ebc0a7491a75ea7dfc0f5c47475dd4
ishmael.ahmed <ishmael.ahmed@readingplus.com>
2017-08-04 09:31:10 -0400

    adding tmux configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,6 @@ mv $HOME/.gvimrc $HOME/.gvimrcbak
 ln -nfs `pwd` $HOME/.vim
 ln -nfs `pwd`/vimrc $HOME/.vimrc
 ln -nfs `pwd`/gvimrc $HOME/.gvimrc
+ln -nfs `pwd`/tmux.conf $HOME/.tmux.conf
 git submodule init
 git submodule update
